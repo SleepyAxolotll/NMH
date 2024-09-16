b3408dc49e1a58fe69afd51d20b182e995ee0a01
Joelle Yang <94316082+d3ltaV@users.noreply.github.com>
2024-09-15 20:06:41 -0400

    Update s1open2022.cpp

--- s1open2022.cpp
+++ s1open2022.cpp
@@ -9,7 +9,7 @@ using namespace std;
 //DSU only works fo undirected graphs; DFS for all cyclic components of graph.
 
 vector<bool> visited;
-pair<int, int> dfs(int node, int parent, vector<vector<pair<int, int>>> adj, int total, int min) {
+pair<int, int> dfs(int node, vector<vector<pair<int, int>>> &adj, int &total, int &min, int parent) {
     visited[node] = true;
     for (pair<int, int> child : adj.at(node)) {
         if (!visited[child.first]) {
@@ -17,7 +17,12 @@ pair<int, int> dfs(int node, int parent, vector<vector<pair<int, int>>> adj, int
             if (child.second < min) {
                 min = child.second;
             }
-            dfs(child.first, node, adj, total, min);
+            dfs(child.first, adj, total, min, parent);
+        } else if (child.first == parent) {
+            total += child.second;
+            if (child.second < min) {
+                min = child.second;
+            }
         }
     }
     return make_pair(total, min);
@@ -29,19 +34,18 @@ int main() {
     for (int i = 0; i < n; i++) {
         int a, b;
         scanf("%d %d", &a, &b);
-        adj.at(i).push_back(make_pair(a, b));
+        adj.at(i).push_back(make_pair(a - 1, b));
     }
     int ans = 0;
     visited.resize(n, false);
-    cout << "hi";
     for (int i = 0; i < n; i++) {
-        cout << "hii";
         if (!visited[i]) {
             int totalWeight = 0;
             int minEdge = INT_MAX;
-            pair<int, int> result = dfs(i, -1, adj, totalWeight, minEdge);
+            pair<int, int> result = dfs(i, adj, totalWeight, minEdge, i);
+            // cout << result.first <<" " << result.second << " ";
             ans += (result.first - result.second);
         }
     }
-    printf("%d\n", ans);
+    printf("%d", ans);
 }
